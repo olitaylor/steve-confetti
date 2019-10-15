2152cc9047c34e619023300371fd3a837c736bf5
olitaylor <olivertaylor18@hotmail.co.uk>
2019-10-15 15:52:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+  </main>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import VueConfetti from 'vue-confetti'
+
+  Vue.use(VueConfetti)
+
+  export default {
+    methods: {
+      start() {
+        this.$confetti.start({
+          particles: [
+            {
+              type: 'image',
+              size: 20,
+              url: 'https://skypanel1.s3-eu-west-1.amazonaws.com/media_files/5cf6d33701062_Steve-Smiling.png',
+            }
+          ]
+        });
+      },
+      stop() {
+        this.$confetti.stop();
+      },
+    }
+  }
+</script>
