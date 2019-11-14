191afa2d31f8281b58100ee438dc5dd3cbfb8906
olitaylor <olivertaylor18@hotmail.co.uk>
2019-11-14 12:54:52 +0000

    parkingfetti

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <main>
     <button @click="startSteve">Start stevefetti</button>
     <button @click="start">Start confetti</button>
+    <button @click="startParking">Start parking</button>
     <button @click="stop">Stop</button>
   </main>
 </template>
@@ -17,12 +18,34 @@
       startSteve() {
         this.$confetti.start({
           particles: [
+
             {
               type: 'image',
               size: 20,
               url: 'https://skypanel1.s3-eu-west-1.amazonaws.com/media_files/5cf6d33701062_Steve-Smiling.png',
               dropRate: 8
+            },
+            {
+              type: 'circle'
+            },
+          ]
+        });
+      },
+      startParking() {
+        this.$confetti.start({
+          particles: [
+            {
+              type: 'circle'
+            },
+            {
+              type: 'image',
+              size: 20,
+              url: 'https://s3.eu-west-2.amazonaws.com/web-assets-1.skypark.dev/images/Parking-Only-Sign-PNG-Free-Image.png',
+              dropRate: 8
             }
+          ],
+          defaultColors: [
+            '#f6ab2f'
           ]
         });
       },
